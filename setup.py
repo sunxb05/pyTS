1e3521fbed963a9ff0e345a4ec020e6f2773a5ea
xiaobo <xiaobosun@Xiaobos-MacBook-Pro.local>
2021-11-04 12:20:33 +0100

    doc test

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ import os
 
 from setuptools import find_packages, setup
 
-from tfn import __author__, __description__, __email__, __version__
+from pyts import __author__, __description__, __email__, __version__
 
 setup(
-    name="tensor-field-networks",
+    name="pyts",
     author=__author__,
     author_email=__email__,
     version=__version__,
